aea095cc619eae4a3872f592fb3bb286ab673fe9
sp1802 <115718222+sp1802@users.noreply.github.com>
2023-02-10 22:14:17 +0530

    Update loss.py

--- utils/loss.py
+++ utils/loss.py
@@ -753,7 +753,6 @@ class ComputeLossOTA:
                 _, cost_argmin = torch.min(cost[:, anchor_matching_gt > 1], dim=0)
                 matching_matrix[:, anchor_matching_gt > 1] *= 0.0
                 matching_matrix[cost_argmin, anchor_matching_gt > 1] = 1.0
-            fg_mask_inboxes = matching_matrix.sum(0) > 0.0
             fg_mask_inboxes = fg_mask_inboxes.to(torch.device('cuda'))
             matched_gt_inds = matching_matrix[:, fg_mask_inboxes].argmax(0)
         
